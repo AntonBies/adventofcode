8afcf5d125e62bc3c9daed49e4c30216e4a6bf8c
Anton Bies <antonbies@gmail.com>
2023-12-07 12:16:17 +0100

    start of class version day3

--- 2023/day3class/index.js
+++ 2023/day3class/index.js
@@ -1,48 +1,51 @@
 const fs = require("fs");
 const path = require("path");
 
+class PartNumber {
+    constructor(number, coordinates) {
+        this.number = number;
+        this.coordinates = coordinates;
+        this.adjacentSymbols = [];
+    }
+}
+
+class PartSymbol {
+    constructor(symbol, i, j) {
+        this.symbol = symbol;
+        this.coordinate = `${i}-${j}`;
+        this.adjacent = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]].map(([di,dj]) => `${i+di}-${j+dj}`);
+        this.adjacentNumbers = [];
+    }
+}
+
 const data = fs
     .readFileSync(path.resolve(__dirname,"input.txt"), { encoding: "utf-8" })
     .trim()
     .split("\n");
 
-const getAdjecentCells = (([i,j]) => [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]].map(([di,dj]) => `${i+di}-${j+dj}`));
+const rowLen = data[0].length;
+const colLen = data.length;
+const numbers = [];
+const symbols = [];
 
-const getNumbersAndSymbols = (input) => {
-    const rowLen = input[0].length;
-    const colLen = input.length;
-    const numbers = [];
-    const symbols = [];
-
-    for (let i = 0; i < rowLen; i++) {
-        for (let j = 0; j < colLen; j++) {
-            const cur = input[i][j];
-            if (cur.match(/[^\d.]/)) {
-                const adj = getAdjecentCells([i,j]);
-                symbols.push({symbol: cur, coordinate: `${i}-${j}`, adjacent: adj});
-                continue;
-            }
-            if (isNaN(cur)) continue;
-            let n = cur;
-            const c = [`${i}-${j}`];
-            while (++j < colLen) {
-                if (isNaN(input[i][j])) break;
-                n += input[i][j];
-                c.push(`${i}-${j}`)
-            }
-            --j;
-            numbers.push({number: n, coordinates: c});
+for (let i = 0; i < rowLen; i++) {
+    for (let j = 0; j < colLen; j++) {
+        const cur = data[i][j];
+        if (cur.match(/[^\d.]/)) {
+            symbols.push(new PartSymbol(cur, i, j));
+            continue;
         }
+        if (isNaN(cur)) continue;
+        let n = cur;
+        const c = [`${i}-${j}`];
+        while (++j < colLen) {
+            if (isNaN(data[i][j])) break;
+            n += data[i][j];
+            c.push(`${i}-${j}`)
+        }
+        --j;
+        numbers.push(new PartNumber(n, c));
     }
-    return {numbers, symbols};
-}
-
-const {numbers, symbols } = getNumbersAndSymbols(data);
-for (const num of numbers) {
-    num["adjacentSymbols"] = [];
-}
-for (const sym of symbols) {
-    sym["adjacentNumbers"] = [];
 }
 
 const partone = numbers.reduce((acc, cur) => {
